4a0c98ba705f0137da196666327d0dd18baa93dd
datara <alexeylos2002@mail.ru>
2024-11-21 11:01:44 +0300

    upd main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 
     content = read_file(FILE_NAME)
     print(f"File content:\n{content}")
-    print("hello")
+    print("hello world привет мир")
 
 if __name__ == "__main__":
     main()
